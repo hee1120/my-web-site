f6b806e7762eb7ef0b88608eecf982481aaf0a24
hee1120 <fijuni1120@gmail.com>
2023-10-05 08:25:37 +0900

    feat: ナビゲーションリストを作成

---
+++ src/types/nav-list.ts
@@ -0,0 +1,25 @@
+type Navlist = {
+  name: string;
+  url: string;
+};
+
+const navList: Navlist[] = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Profile",
+    url: "/profile",
+  },
+  {
+    name: "Works",
+    url: "/works",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
+];
+
+export default navList;
